0f6964ebaeb6bdf159e892e960823bc0e8fa3f55
user <krysanmihasan@gmail.com>
2022-05-17 17:10:03 +0300

    second commit - added buttons, top&bottom layouts

--- app/src/main/java/com/appbymikekrysan/homework_19/MainActivity.kt
+++ app/src/main/java/com/appbymikekrysan/homework_19/MainActivity.kt
@@ -2,10 +2,15 @@ package com.appbymikekrysan.homework_19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val content = findViewById<ViewGroup>(R.id.root)
+        val view = layoutInflater.inflate(R.layout.item, content, false)
+        content.addView(view)
     }
 }
